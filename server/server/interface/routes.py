b219c5b35c9e6909f6a693c6634263ff9dd75ee3
Tim Baccaert <timbaccaert@gmail.com>
2017-07-26 13:34:46 +0200

    derp2

--- server/server/interface/routes.py
+++ server/server/interface/routes.py
@@ -226,7 +226,7 @@ def rate_route(request):
             return edge
 
         logging.info("RATING: mapping ratings to the graph edges")
-        GRAPH.map_graph(lambda _, e: update_rating)
+        GRAPH.map_graph(update_rating)
         return HttpResponse('')
     except:
         logging.error("RATE: this is the error: %s", e)
